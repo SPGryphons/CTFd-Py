7de8c0bd44718dea983cc48b181c431767362384
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2023-05-29 22:02:25 +0800

    Create constants.py

---
+++ CTFdPy/constants.py
@@ -0,0 +1,16 @@
+# Challenge constants
+
+class ChallengeType:
+    standard = "standard"
+    dynamic = "dynamic"
+
+class ChallengeState:
+    hidden = "hidden"
+    visible = "visible"
+
+
+# Flag constants
+
+class FlagType:
+    static = "static"
+    regex = "regex"
