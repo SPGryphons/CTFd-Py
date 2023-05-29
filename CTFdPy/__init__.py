5e2e644fc6fecb01ed1d10f6e481bc113ce4f0fd
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2023-05-29 22:02:39 +0800

    Create __init__.py

---
+++ CTFdPy/__init__.py
@@ -0,0 +1,9 @@
+from CTFdPy.client import Client
+from CTFdPy.constants import ChallengeType, ChallengeState, FlagType
+
+__all__ = (
+    "Client",
+    "ChallengeType",
+    "ChallengeState",
+    "FlagType"
+)
